b0566cedb388041080041e8cf4956888fb174ecd
thetruehoney <d.d.czachowski@edu.salford.ac.uk>
2022-01-12 19:41:44 +0000

    Bootstrap

--- MVCtemplate/js/custom-js.js
+++ MVCtemplate/js/custom-js.js
@@ -1,9 +1,12 @@
 function slideIn() {
-    document.getElementById("slider").style.display = "block";
-    document.getElementById("slider").style.width = "fit-content";
+    document.getElementById("slider").classList.toggle("show");
+    document.getElementById("slider").classList.remove("hide");
+    if (document.getElementById("slider").classList.contains("marginOut")) {
+        document.getElementById("slider").classList.toggle("marginOut");
+    }
 }
 
 function slideOut() {
-    document.getElementById("slider").style.width = "0";
-    document.getElementById("slider").style.display = "none";
+    document.getElementById("slider").classList.toggle("marginOut");
+    document.getElementById("slider").classList.remove("show");
 }
