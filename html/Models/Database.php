8913017b804271723bd367366021c868e08cba2e
SZT99 <63964406+SZT99@users.noreply.github.com>
2022-07-17 11:53:58 +0100

    Uploaded sanitised Database.php

---
+++ html/Models/Database.php
@@ -0,0 +1,58 @@
+<?php
+
+class Database {
+    /**
+     * @var Database
+     */
+    protected static $_dbInstance = null;
+
+    /**
+     * @var PDO
+     */
+    protected $_dbHandle;
+
+    /**
+     * @return Database
+     */
+    public static function getInstance() {
+        $username ='_ADD_USERNAME_HERE__';
+        $password = '_ADD_PASSWORD_HERE__';
+        $host = '_ADD_HOSTNAME_OR_IP_HERE__';
+        $dbName = '_ADD_DATABASE_NAME_HERE__';
+       
+        if(self::$_dbInstance === null) { //checks if the PDO exists
+            // creates new instance if not, sending in connection info
+            self::$_dbInstance = new self($username, $password, $host, $dbName);
+        }
+
+        return self::$_dbInstance;
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @param $host
+     * @param $database
+     */
+    private function __construct($username, $password, $host, $database) {
+        try { 
+            $this->_dbHandle = new PDO("mysql:host=$host;dbname=$database",  $username, $password); // creates the database handle with connection info
+        //$this->_dbHandle = new PDO('mysql:host=' . $host . ';dbname=' . $database,  $username, $password); // creates the database handle with connection info
+           
+        }
+        catch (PDOException $e) { // catch any failure to connect to the database
+	    echo $e->getMessage();
+	}
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getdbConnection() {
+        return $this->_dbHandle; // returns the PDO handle to be used elsewhere
+    }
+
+    public function __destruct() {
+        $this->_dbHandle = null; // destroys the PDO handle when no longer needed
+    }
+}
